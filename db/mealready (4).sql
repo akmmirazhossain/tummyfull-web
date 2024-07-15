c72e18adc4ef47ae4e24182367a52bc794c829f5
AKM Miraz Hossain <akmmirazhossain@gmail.com>
2024-07-15 09:48:23 +0600

    Delivery section dropdown done

--- db/mealready (4).sql
+++ db/mealready (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 11, 2024 at 04:36 AM
+-- Generation Time: Jul 15, 2024 at 03:47 AM
 -- Server version: 8.0.36
 -- PHP Version: 8.2.12
 
@@ -61,8 +61,8 @@ CREATE TABLE `mrd_delivery` (
 --
 
 INSERT INTO `mrd_delivery` (`mrd_delivery_id`, `mrd_delivery_user_id`, `mrd_delivery_status`, `mrd_delivery_order_id`, `mrd_delivery_message_type`, `mrd_delivery_message`, `mrd_delivery_date_updated`) VALUES
-(4, 1, 1, NULL, 'mealbox', 'mealbox activated', '2024-07-06 21:41:33'),
-(5, 18, 1, NULL, 'mealbox', 'mealbox activated', '2024-07-06 06:22:59'),
+(4, 1, 1, NULL, 'mealbox', 'mealbox activated', '2024-07-12 09:25:13'),
+(5, 18, 1, NULL, 'mealbox', 'mealbox activated', '2024-07-14 14:06:44'),
 (6, NULL, 0, NULL, 'mealbox', 'mealbox deactivated', '2024-07-03 04:36:50');
 
 -- --------------------------------------------------------
@@ -153,10 +153,28 @@ CREATE TABLE `mrd_notification` (
   `mrd_notif_user_id` int NOT NULL,
   `mrd_notif_message` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `mrd_notif_type` enum('order','mealbox','delivery') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `mrd_notif_credit_calc` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `mrd_notif_status` int NOT NULL DEFAULT '0',
   `mrd_notif_date_added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `mrd_notif_data_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mrd_notification`
+--
+
+INSERT INTO `mrd_notification` (`mrd_notif_id`, `mrd_notif_user_id`, `mrd_notif_message`, `mrd_notif_type`, `mrd_notif_credit_calc`, `mrd_notif_status`, `mrd_notif_date_added`, `mrd_notif_data_updated`) VALUES
+(1, 1, 'Ordered lunch of July 21 (Sunday)', 'order', NULL, 0, '2024-07-14 13:55:21', '2024-07-14 13:55:21'),
+(2, 1, 'Ordered lunch of July 15 (Monday)', 'order', NULL, 0, '2024-07-14 13:55:43', '2024-07-14 13:55:43'),
+(3, 1, 'Ordered lunch of July 16 (Tuesday)', 'order', NULL, 0, '2024-07-14 13:56:00', '2024-07-14 13:56:00'),
+(4, 18, 'Ordered lunch of July 16 (Tuesday)', 'order', NULL, 0, '2024-07-14 13:59:18', '2024-07-14 13:59:18'),
+(5, 18, 'Ordered lunch of July 18 (Thursday)', 'order', NULL, 0, '2024-07-14 13:59:20', '2024-07-14 13:59:20'),
+(6, 18, 'Deactivated mealbox, Tk 150 will be refunded.', 'mealbox', NULL, 0, '2024-07-14 14:06:37', '2024-07-14 14:06:37'),
+(7, 18, 'Activated mealbox for TK 150.', 'mealbox', NULL, 0, '2024-07-14 14:06:44', '2024-07-14 14:06:44'),
+(8, 1, 'Your lunch has been delivered.', 'order', '995 - 120 = 875', 0, '2024-07-14 14:08:37', '2024-07-14 14:08:37'),
+(9, 1, 'Your lunch has been delivered.', 'order', '875 - 297 = 578', 0, '2024-07-14 14:14:26', '2024-07-14 14:14:26'),
+(10, 1, 'Ordered lunch of July 18 (Thursday)', 'order', NULL, 0, '2024-07-14 14:21:03', '2024-07-14 14:21:03'),
+(11, 1, 'Your lunch has been delivered. Quantity (5)', 'order', '578 - 495 = 83', 0, '2024-07-14 14:25:13', '2024-07-14 14:25:13');
 
 -- --------------------------------------------------------
 
@@ -184,39 +202,13 @@ CREATE TABLE `mrd_order` (
 --
 
 INSERT INTO `mrd_order` (`mrd_order_id`, `mrd_order_user_id`, `mrd_order_menu_id`, `mrd_order_quantity`, `mrd_order_discount`, `mrd_order_total_price`, `mrd_order_status`, `mrd_order_mealbox`, `mrd_order_date`, `mrd_order_rating`, `mrd_order_feedback`, `mrd_order_date_insert`) VALUES
-(1, '1', 1, '1', NULL, 130, 'pending', 1, '2024-07-07', NULL, NULL, '2024-07-06 12:06:24'),
-(2, '1', 5, '1', NULL, 110, 'pending', 1, '2024-07-09', NULL, NULL, '2024-07-07 03:41:33'),
-(3, '18', 13, '1', NULL, 95, 'pending', 1, '2024-07-06', NULL, NULL, '2024-07-06 09:19:12'),
-(4, '18', 1, '1', NULL, 130, 'pending', 1, '2024-07-07', NULL, NULL, '2024-07-07 04:26:46'),
-(5, '18', 3, '1', NULL, 120, 'cancelled', 1, '2024-07-08', NULL, NULL, '2024-07-06 12:29:41'),
-(6, '1', 13, '1', NULL, 95, 'cancelled', 1, '2024-07-06', NULL, NULL, '2024-07-06 09:59:05'),
-(7, '1', 14, '1', NULL, 110, 'cancelled', 1, '2024-07-06', NULL, NULL, '2024-07-06 09:59:13'),
-(8, '1', 2, '1', NULL, 115, 'cancelled', 1, '2024-07-07', NULL, NULL, '2024-07-06 12:06:24'),
-(9, '1', 4, '1', NULL, 110, 'pending', 1, '2024-07-08', NULL, NULL, '2024-07-07 03:41:33'),
-(10, '1', 6, '1', NULL, 105, 'pending', 1, '2024-07-09', NULL, NULL, '2024-07-07 03:41:33'),
-(11, '1', 8, '1', NULL, 100, 'pending', 1, '2024-07-10', NULL, NULL, '2024-07-07 03:41:33'),
-(12, '18', 14, '1', NULL, 110, 'pending', 0, '2024-07-06', NULL, NULL, '2024-07-06 12:22:05'),
-(13, '1', 1, '1', NULL, 130, 'pending', 1, '2024-07-07', NULL, NULL, '2024-07-06 12:06:24'),
-(14, '18', 4, '1', NULL, 110, 'cancelled', 1, '2024-07-08', NULL, NULL, '2024-07-06 12:23:36'),
-(15, '18', 2, '1', NULL, 115, 'cancelled', 1, '2024-07-07', NULL, NULL, '2024-07-06 12:29:39'),
-(16, '18', 8, '1', NULL, 100, 'cancelled', 1, '2024-07-10', NULL, NULL, '2024-07-06 12:23:51'),
-(17, '1', 3, '1', NULL, 120, 'pending', 1, '2024-07-08', NULL, NULL, '2024-07-07 03:41:33'),
-(18, '1', 10, '1', NULL, 120, 'pending', 1, '2024-07-11', NULL, NULL, '2024-07-07 03:41:33'),
-(19, '1', 11, '1', NULL, 105, 'pending', 1, '2024-07-12', NULL, NULL, '2024-07-07 03:41:33'),
-(20, '18', 5, '1', NULL, 110, 'cancelled', 1, '2024-07-09', NULL, NULL, '2024-07-06 12:23:39'),
-(21, '18', 11, '1', NULL, 105, 'cancelled', 1, '2024-07-12', NULL, NULL, '2024-07-06 12:22:59'),
-(22, '18', 6, '1', NULL, 105, 'cancelled', 1, '2024-07-09', NULL, NULL, '2024-07-06 12:22:59'),
-(23, '18', 12, '1', NULL, 125, 'cancelled', 1, '2024-07-12', NULL, NULL, '2024-07-06 12:23:48'),
-(24, '18', 10, '1', NULL, 120, 'pending', 1, '2024-07-11', NULL, NULL, '2024-07-07 04:26:43'),
-(25, '18', 9, '1', NULL, 110, 'cancelled', 1, '2024-07-11', NULL, NULL, '2024-07-06 12:23:50'),
-(26, '1', 6, '1', NULL, 105, 'pending', 1, '2024-07-16', NULL, NULL, '2024-07-10 01:33:44'),
-(27, '1', 3, '1', NULL, 120, 'pending', 1, '2024-07-15', NULL, NULL, '2024-07-10 01:33:45'),
-(28, '1', 2, '1', NULL, 115, 'cancelled', 1, '2024-07-14', NULL, NULL, '2024-07-10 01:42:50'),
-(29, '1', 1, '1', NULL, 130, 'pending', 1, '2024-07-14', NULL, NULL, '2024-07-10 01:42:48'),
-(30, '1', 9, '1', NULL, 110, 'pending', 1, '2024-07-11', NULL, NULL, '2024-07-11 07:27:05'),
-(31, '1', 14, '1', NULL, 110, 'pending', 1, '2024-07-13', NULL, NULL, '2024-07-10 02:50:26'),
-(32, '1', 7, '1', NULL, 110, 'cancelled', 1, '2024-07-10', NULL, NULL, '2024-07-10 03:02:55'),
-(33, '1', 13, '1', NULL, 95, 'cancelled', 1, '2024-07-13', NULL, NULL, '2024-07-11 08:26:30');
+(1, '1', 1, '1', NULL, 130, 'pending', 1, '2024-07-21', NULL, NULL, '2024-07-14 19:55:20'),
+(2, '1', 3, '1', NULL, 120, 'delivered', 1, '2024-07-15', NULL, NULL, '2024-07-14 20:08:37'),
+(3, '1', 5, '3', NULL, 297, 'delivered', 1, '2024-07-16', NULL, NULL, '2024-07-14 20:14:26'),
+(4, '18', 5, '1', NULL, 110, 'pending', 1, '2024-07-16', NULL, NULL, '2024-07-14 20:06:44'),
+(5, '18', 9, '1', NULL, 110, 'pending', 1, '2024-07-18', NULL, NULL, '2024-07-14 20:06:44'),
+(6, '1', 9, '5', NULL, 495, 'delivered', 1, '2024-07-18', NULL, NULL, '2024-07-14 20:25:13'),
+(7, '1', 4, '3', NULL, 297, 'pending', 1, '2024-07-15', NULL, NULL, '2024-07-15 08:15:08');
 
 -- --------------------------------------------------------
 
@@ -289,9 +281,9 @@ CREATE TABLE `mrd_user` (
 --
 
 INSERT INTO `mrd_user` (`mrd_user_id`, `mrd_user_type`, `mrd_user_last_otp`, `mrd_user_otp_expiration`, `mrd_user_otp_attempts`, `mrd_user_session_token`, `mrd_user_group_id`, `mrd_user_first_name`, `mrd_user_last_name`, `mrd_user_image_file_name`, `mrd_user_phone`, `mrd_user_verified`, `mrd_user_email`, `mrd_user_password`, `mrd_user_address`, `mrd_user_sector_block`, `mrd_user_area`, `mrd_user_payment_phone`, `mrd_user_status`, `mrd_user_meal_prep_ask`, `mrd_user_mealbox`, `mrd_user_total_meal`, `mrd_user_delivery_ask`, `mrd_user_delivery_instruction`, `mrd_user_meal_size`, `mrd_user_credit`, `mrd_user_credit_to_pay`, `mrd_user_date_added`, `updated_at`) VALUES
-(1, 'customer', '$2y$12$1Q75HSt4tiv02WzbDRSt4.2QGXLC6JzJLlWI5D1wSj45oD3/vlTWC', '2024-06-26 09:27:35', '0', '46743aeebdc5e2ca6455afe47f4f304083bed933fa919456898a24b7fc8e48f7', 1, 'AKM Miraz Hoss', 'Hossain', '3245t464.jpg', '01673692998', 0, 'akmmirazhossain@gmail.com', '$2y$12$Ha9G2nkjFnGr9oW4C3OxouVxiz4bz.F8iQCxwUJ0.XxTSVH/LCqoq', 'B4, House 124, Road 12, Blo', '', 1, '01673692997', NULL, NULL, 1, 50, 'yes', 'AMI AKM আমি জাহাজের ভেতরে সমুদ্রে থাকি আমার খাবারটা ড্রোন দিয়ে ডেলিভারি দিয়েন', 'L', '1350', '2:245|3:587', '46232', '2024-07-06 22:24:25'),
+(1, 'customer', '$2y$12$1Q75HSt4tiv02WzbDRSt4.2QGXLC6JzJLlWI5D1wSj45oD3/vlTWC', '2024-06-26 09:27:35', '0', '46743aeebdc5e2ca6455afe47f4f304083bed933fa919456898a24b7fc8e48f7', 1, 'AKM Miraz Hoss', 'Hossain', '3245t464.jpg', '01673692998', 0, 'akmmirazhossain@gmail.com', '$2y$12$Ha9G2nkjFnGr9oW4C3OxouVxiz4bz.F8iQCxwUJ0.XxTSVH/LCqoq', 'B4, House 124, Road 12, Blo', '', 1, '01673692997', NULL, NULL, 1, 50, 'yes', 'AMI AKM আমি জাহাজের ভেতরে সমুদ্রে থাকি আমার খাবারটা ড্রোন দিয়ে ডেলিভারি দিয়েন', 'L', '1500', '2:245|3:587', '46232', '2024-07-14 14:28:25'),
 (16, 'chef', '', '', '', '1234', 0, 'TF Chef', '0', '0', '', 0, '0', '0', '0', '', 2, '0', NULL, NULL, 0, 0, '', NULL, '0', '0', '0', '', '2024-07-03 02:39:17'),
-(18, 'customer', '$2y$12$RbKmMp4aT3I.rpmOQgk3TesD5z1yoirlbhT2VzXJpYWnQKBAKnVSq', '2024-07-07 04:30:00', '0', 'decdb6a8ae6021dba6b5e414196256925b201c05d9685f74b80e4920dab856aa', NULL, 'Thatch Bhai', '0', '0', '01673692997', NULL, '0', '0', 'I live in the seajsjssisiajsisiajxuxudsisjsisissisisisi is jajsis', NULL, 2, '0', NULL, NULL, 1, NULL, NULL, 'আমি জাহাজের ভেতরে সমুদ্রে থাকি আমার খাবারটা ড্রোন দিয়ে ডেলিভারি দিয়েন', '0', '0', '0', '2024-06-29 11:07:18', '2024-07-06 22:25:00');
+(18, 'customer', '$2y$12$RbKmMp4aT3I.rpmOQgk3TesD5z1yoirlbhT2VzXJpYWnQKBAKnVSq', '2024-07-07 04:30:00', '0', 'decdb6a8ae6021dba6b5e414196256925b201c05d9685f74b80e4920dab856aa', NULL, 'Thatch Bhai', '0', '0', '01673692997', NULL, '0', '0', 'I live in the sea', NULL, 2, '0', NULL, NULL, 1, NULL, NULL, 'আমি জাহাজের ভেতরে সমুদ্রে থাকি আমার খাবারটা ড্রোন দিয়ে ডেলিভারি দিয়েন', '0', '500', '0', '2024-06-29 11:07:18', '2024-07-14 14:28:21');
 
 --
 -- Indexes for dumped tables
@@ -389,13 +381,13 @@ ALTER TABLE `mrd_menu`
 -- AUTO_INCREMENT for table `mrd_notification`
 --
 ALTER TABLE `mrd_notification`
-  MODIFY `mrd_notif_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `mrd_notif_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `mrd_order`
 --
 ALTER TABLE `mrd_order`
-  MODIFY `mrd_order_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `mrd_order_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `mrd_setting`
