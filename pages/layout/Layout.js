c1a61d592425c4ab8430862d25535ca79081ff71
AKM Miraz Hossain <akmmirazhossain@gmail.com>
2024-08-27 01:35:03 +0600

    OG details added for the homepage

--- pages/layout/Layout.js
+++ pages/layout/Layout.js
@@ -9,6 +9,16 @@ const Layout = ({ children, title }) => {
     <div className="bg_light_orange text_black">
       <Head>
         <title>{title ? title : "ডালভাত"}</title>
+        <meta name="description" content="Your page description goes here." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.dalbhath.com/" />
+        <meta property="og:title" content="ডালভাত.com" />
+        <meta
+          property="og:description"
+          content="DalBhath is a meal management app that helps college students living in shared apartments easily plan and manage their daily lunch and dinner, ensuring they always have a meal without the hassle of cooking or organizing."
+        />
+        <meta property="og:image" content="https://dalbhath.com/logo.png" />
       </Head>
       <header className="fixed w-full z-20">
         <NavbarTop className="" />
