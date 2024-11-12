b2294f31f3edff6bd44e544a70e59e56866e4bdc
AKM Miraz Hossain <akmmirazhossain@gmail.com>
2024-11-13 02:16:20 +0600

    home page card chages

--- pages/components/menu.js
+++ pages/components/menu.js
@@ -17,6 +17,8 @@ import { formatDate } from "../../lib/formatDate";
 import { useNotification } from "../contexts/NotificationContext";
 import axios from "axios";
 import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+dayjs.extend(customParseFormat);
 import { ApiContext } from "../contexts/ApiContext";
 
 const IOSSwitch = styled((props) => (
@@ -471,10 +473,25 @@ const MenuComp = () => {
                       <div className="h2_akm pl_akm">
                         <span>Lunch</span>
                       </div>
-                      <div className="h4_akm pr_akm">
-                        <FontAwesomeIcon icon={faShippingFast} />{" "}
-                        {settings?.delivery_time_lunch || "N/A"}
-                      </div>
+
+                      {firstDay === day &&
+                        lunchOrderAcceptText &&
+                        menuData[day].lunch.status !== "enabled" && (
+                          <div className="h4_akm md:pr-4 flex items-center gap-0.5">
+                            <svg
+                              className="w-4"
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 -960 960 960"
+                              fill="currentColor"
+                            >
+                              <path d="M280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM246-720l96 200h280l110-200H246Zm-38-80h590q23 0 35 20.5t1 41.5L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68-39.5t-2-78.5l54-98-144-304H40v-80h130l38 80Zm134 280h280-280Z" />
+                            </svg>
+                            Until{" "}
+                            {dayjs(settings.time_limit_lunch, "hh:mm a").format(
+                              "h a"
+                            )}
+                          </div>
+                        )}
                     </div>
                     <div className="relative grid grid-cols-2  p-2 lg:p-12 h-auto border-y-1 ">
                       {menuData[day].lunch.foods.map((food, index) => (
